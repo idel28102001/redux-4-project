37fd3727248c4e08dff40efe2cf692c0c262d667
Idel Gataullin <idel28102001@mail.ru>
2023-05-26 11:36:56 +0300

    no error for sure

--- src/features/auth/components/AuthGroup/AuthGroup.tsx
+++ src/features/auth/components/AuthGroup/AuthGroup.tsx
@@ -2,10 +2,11 @@ import styles from './AuthGroup.module.scss';
 import AuthenticatedGroup from '../AuthenticatedGroup';
 import { useAppSelector } from '@/hooks/useStoreHooks.ts';
 import { selectAuth } from '@/store/reducers/auth';
+import NotAuthenticatedGroup from '@/features/auth/components/NotAuthenticatedGroup';
 
 const AuthGroup = () => {
   const { isAuth } = useAppSelector(selectAuth);
-  const content = isAuth ? <AuthenticatedGroup /> : <AuthenticatedGroup />;
+  const content = isAuth ? <AuthenticatedGroup /> : <NotAuthenticatedGroup />;
   return <div className={styles.root}>{content}</div>;
 };
 
